abb02266ee0a1ca8ffdfb91aa0536f48e8432644
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-09-24 21:53:12 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,14 @@
 class PostsController < ApplicationController
 	def index
 		@posts = Post.all
-		render :index
 	end
 
 	def show
 		@post = Post.find(params[:id])
-		render :show
 	end
 
 	def new
 		@post = Post.new
-		render :new
 	end
 
 	def create
